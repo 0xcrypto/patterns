4e98191574bffd040a7e783b25f011b28e4c4599
Vikrant Singh Chauhan <vikrantcode@gmail.com>
2018-04-13 11:50:09 +0530

    Better calc

--- calc.js
+++ calc.js
@@ -1,2 +1,5 @@
 // One line calculator
 calc = () => { return eval(prompt().replace(/[^\ \+\-\*\/0-9]/g, '')); }
+
+// even better...
+calc = (str) => { return eval( (str ? str : prompt()).replace(/[^\ \+\-\*\/0-9]/g, '')) || false; }
